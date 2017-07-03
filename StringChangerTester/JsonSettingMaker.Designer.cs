9dfe127966cf198b92b0c13fc436b5b717004e4c
KronosKillah <sharkydanny@gmail.com>
2017-07-04 01:33:13 +1200

    Improved JSON Function maker thing
    made it easier and better to use, so u can Load a txt file of textbox names and generate the format

--- StringChangerTester/JsonSettingMaker.Designer.cs
+++ StringChangerTester/JsonSettingMaker.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.loadTxtFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox2
@@ -51,11 +55,51 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // loadTxtFile
+            // 
+            this.loadTxtFile.Location = new System.Drawing.Point(12, 232);
+            this.loadTxtFile.Name = "loadTxtFile";
+            this.loadTxtFile.Size = new System.Drawing.Size(75, 23);
+            this.loadTxtFile.TabIndex = 4;
+            this.loadTxtFile.Text = "Load Txt";
+            this.loadTxtFile.UseVisualStyleBackColor = true;
+            this.loadTxtFile.Click += new System.EventHandler(this.loadTxtFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "File Loaded : null";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(93, 236);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(129, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Function Maker mode";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(229, 236);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 20);
+            this.textBox1.TabIndex = 7;
+            // 
             // JsonSettingMaker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(704, 267);
+            this.ClientSize = new System.Drawing.Size(704, 281);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loadTxtFile);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Name = "JsonSettingMaker";
@@ -72,5 +116,9 @@
 
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button loadTxtFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- StringChangerTester/JsonSettingMaker.cs
+++ StringChangerTester/JsonSettingMaker.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace StringChangerTester
 {
@@ -16,61 +17,53 @@ namespace StringChangerTester
         {
             InitializeComponent();
         }
+        public string[] inpute;
+        public bool fileopened = false;
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string[] inp = { "pubmatchBox", "multiplayerBox", "pubmatchDescBox", "storeDescBox", "storeBox", "optionsDescBox", "optionsBox", "barracksDescBox", "barracksBox", "codtvDescBox", "codtvBox", "theaterDescBox", "theaterBox", "customGameDescBox", "customGameBox", "leagueDescBox", "leaguePlayBox", "localBox", "lanPartyBox", "playOnlineBox", "playersOnlineBox", "addSplitscreenControllerBox", "backButtonBox", "friendsPlayingBox", "scorestreaksDescBox", "scorestreaksBox", "createclassDescBox", "createaclassBox", "findmatchDescBox", "findMatchBox", "livestreamDescBox", "livestreamBox", "deadScilenceDescBox", "deadscilenceBox", "tacMaskDescBox", "tacMaskBox", "engineerDescBox", "engineerBox", "exCondDesBox", "exCondBox", "dexterityDescBox", "dexterityBox", "scavengerDescBox", "scavengerBox", "hardwiredDescBox", "hardwiredBox", "fasthandsDescBox", "fasthandsBox", "coldbloodedDescBox", "coldbloodBox", "toughnessDescBox", "toughnessBox", "ghostDescBox", "ghostBox", "flakjacketDescBox", "flakjacketBox", "blindeyeDescBox", "blindeyeBox", "hardlineDescBox", "hardlineBox", "lightweightDescBox", "lightweightBox", "awarenessDescBox", "awarenessBox", "c4DescBox", "c4Box", "bouncingbettyDescBox", "bouncingbettyBox", "combataxeDescBox", "combataxeBox", "semtexDescBox", "semtexBox", "claymoreDescBox", "grenadeDescBox", "claymoreBox", "grenadeBox", "tacinsertDescBox", "tacinsertBox", "trophysystemDescBox", "trophysystemBox", "flashbagDescBox", "flashbangBox", "blackhatDescBox", "blackhatBox", "shockchargeDescBox", "shockchargeBox", "empgrenadeDescBox", "empgrenadeBox", "sensorgrenadeDescBox", "sensorgrenadeBox", "stungrenadeDescBox", "stungrenadeBox", "concussionDescBox", "concussionBox", "assaultriflesBox", "an94DescBox", "an94Box", "maDescBox", "m8a1Box", "smrDescBox", "smrBox", "scarDescBox", "scarhBox", "m27DescBox", "m27Box", "faloswDescBox", "faloswBox", "swat556DescBox", "swat556Box", "type25DescBox", "type25Box", "mtarDescBox", "mtarBox", "m1216DescBox", "m1216Box", "ksgDescBox", "ksgBox", "s12DescBox", "s12Box", "r870mcsDescBox", "r870mcsBox", "HAMRDescBox", "HAMRBox", "LSATDescBox", "LSATBox", "QBBLSWDescBox", "QBBLSWBox", "MK48DescBox", "MK48Box", "XPR50DescBox", "XPR50Box", "BallistaDescBox", "BallistaBox", "DSR50DescBox", "DSR50Box", "SVUASDescBox", "SVUASBox", "assaultshieldDescBox", "assaultshieldBox" };
-            //for (int i = 0; i < inp.Length; i++)
-            //{
-            //    // string input = $"{{'name':'{inp[i].ToString()}','value':''}}\r\n";
-            //    //string input = $"FuncDank.Bo2LoadSettingsTool('{ inp[i].ToString()}',{ inp[i].ToString()}.Text);"+Environment.NewLine;
-            //    //string input = $"if (setting.name == '{ inp[i].ToString()}') {{{ inp[i].ToString()}.Text = setting.value; }};" + Environment.NewLine;
-            //    textBox2.Text += input;
-
-            //}
-
-
-            //string input = textBox1.Text;
-
-
-            //textBox2.Text = input.Replace(Environment.NewLine,", ");
-
-            //string settings_filepath = "Bo2ToolSettings.json";
-            //if (File.Exists(settings_filepath))
-            //{
-            //    string[] Settings_Settings_content = File.ReadAllLines(settings_filepath);
-            //    foreach (string line in Settings_Settings_content)
-            //    {
-            //        Setting setting = JsonConvert.DeserializeObject<Setting>(line);
-            //        settingsList.Add(setting);
-            //        if (setting.name == inp[0].ToString())
-            //        {
-            //            textBox1.Text += setting.value;
-            //        }
-            //        if (setting.name == inp[1].ToString())
-            //        {
-            //            string test = "textBox2.Text";
-            //            test += setting.value;
-            //        }
-            //    }
-            //}
-            //else MessageBox.Show(settings_filepath + " is missing! :(");
-
-            for (int i = 0; i < inp.Length; i++)
+            if (fileopened == true)
             {
-                string input = "";
+                if (checkBox1.Checked)
+                {
+                    textBox2.Clear();
+                    MessageBox.Show(inpute.Length.ToString());
+                    for (int i = 0; i < inpute.Length; i++)
+                    {
+                        
+                        string input = "";
+                        if (textBox1.Text == string.Empty)
+                        {
+                            MessageBox.Show("Please Enter a Saving Function Name", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                        }
+                        else
+                        {
+                            
+                            input = $"{textBox1.Text}(\"{ inpute[i].ToString()}\", {inpute[i].ToString()}.Text);" + Environment.NewLine;
+                            textBox2.Text += input;//System.Diagnostics.Process.Start($"cmd.exe", "echo{input}");
+                        }
+                    }
+                }
+                else
+                {
+                    textBox2.Clear();
+                    for (int i = 0; i < inpute.Length; i++)
+                    {
+                        string input = "";
 
-                input = $"if(setting.name == '{inp[i].ToString()}'){{{ inp[i].ToString()}.Text = setting.value; }}" + Environment.NewLine;
-                textBox2.Text += input;//System.Diagnostics.Process.Start($"cmd.exe", "echo{input}");
+                        input = $"if(setting.name == \"{inpute[i].ToString()}\"){{{ inpute[i].ToString()}.Text = setting.value; }}" + Environment.NewLine;
+                        textBox2.Text += input;//System.Diagnostics.Process.Start($"cmd.exe", "echo{input}");
+                    }
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please load txt file","Errpr!",MessageBoxButtons.OK,MessageBoxIcon.Error);
             }
 
-            for (int i = 0; i < 137; i++)
-            {
 
-                string input2 = inp[i].ToString();
-                //string input =  "{0}.Text,\r\n",input2;
-                //textBox2.Text += input;//System.Diagnostics.Process.Start($"cmd.exe", "echo{input}");
-            }
         }
 
         private void JsonSettingMaker_FormClosed(object sender, FormClosedEventArgs e)
@@ -83,5 +76,26 @@ namespace StringChangerTester
         {
 
         }
+
+        private void loadTxtFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "txt files (*txt)| *.txt";
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    inpute = File.ReadAllLines(ofd.FileName);
+                    MessageBox.Show("Loaded txt file");
+                    fileopened = true;
+                    label1.Text = "File Loaded : " + ofd.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
     }
 }
